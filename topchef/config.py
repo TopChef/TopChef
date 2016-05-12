17c0504184f1fb1f0b1858500634f15783aafdb2
MichalKononenko <michalkononenko@gmail.com>
2016-05-11 20:49:40 -0400

    Don't barf on Travis if unable to load from the /var/tmp directory

--- topchef/config.py
+++ topchef/config.py
@@ -13,7 +13,7 @@ DEBUG = True
 BASE_DIRECTORY = os.path.abspath(os.path.curdir)
 
 # DATABASE
-DATABASE_URI = 'sqlite:////var/tmp/topchef_db.sqlite'
+DATABASE_URI = 'sqlite://var/tmp/db.sqlite3' if not os.environ['DATABASE_URI'] else os.environ['DATABASE_URI']
 
 
 # ROOT USER
