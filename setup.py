0459a1355ecb22bf0620fdcc88c79fa2a31aa765
Michal Kononenko <michalkononenko@gmail.com>
2016-07-19 12:33:40 -0400

    Added rudimentary setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='topchef',
+      version='0.1',
+      description='Python library for running remote computing jobs',
+      author='Michal Kononenko',
+      author_email='mkononen@uwaterloo.ca',
+      packages=['topchef']
+) 
