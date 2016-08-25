e5c11c872c75118c7aef71c1173f41bcac033f2f
Michal Kononenko <michalkononenko@gmail.com>
2016-08-25 13:21:01 -0400

    Added JSON Schema validator endpoint

--- tests/acceptance/test_api.py
+++ tests/acceptance/test_api.py
@@ -33,6 +33,32 @@ def flask_app():
     yield base_url
     
     server.terminate()
+    
+    time.sleep(3)
+
+    assert not server.is_alive()
+
+@pytest.fixture
+def json_schema():
+    schema = {
+        'type': 'object',
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        'properties': {
+            'number': {
+                'type': 'integer',
+                'minimum': 0,
+                'maximum': 2
+            },
+            'string': {
+                'type': 'string'
+            },
+            'boolean': {
+                'type': 'boolean'
+            }
+        }
+    }
+
+    return schema
 
 @pytest.fixture
 def valid_json():
@@ -41,7 +67,7 @@ def valid_json():
         'string': 'string',
         'boolean': True
     }
-    return json.dumps(json_dict)
+    return json_dict
 
 def test_is_server_up(flask_app):
     response = requests.get(
@@ -53,12 +79,42 @@ def test_json_loopback(flask_app, valid_json):
     url = '%s/echo' % flask_app
     response = requests.post(
         url, headers={'Content-Type': 'application/json'},
-        data=valid_json
+        data=json.dumps(valid_json)
     )
 
     assert response.status_code == 200
 
     response_from_server = response.json()
 
-    assert response_from_server['data'] == json.loads(valid_json)
+    assert response_from_server['data'] == valid_json
 
+class TestJSONSchemaValidator(object):
+    endpoint = '/validator'
+
+    def test_validator_200(self, flask_app, json_schema, valid_json):
+        url = '%s%s' % (flask_app, self.endpoint)
+
+        data_to_post = {'object': valid_json, 'schema': json_schema}
+
+        response = requests.post(
+            url, headers={'Content-Type': 'application/json'},
+            data=json.dumps(data_to_post)
+        )
+
+        assert response.status_code == 200
+
+    def test_validator_400(self, flask_app, json_schema):
+        invalid_json = {'number': 1, 'string': 2, 'boolean': True}
+
+        url = '%s%s' % (flask_app, self.endpoint)
+
+        data_to_post = {'object': invalid_json, 'schema': json_schema}
+
+        response = requests.post(
+            url, headers={'Content-Type': 'application/json'},
+            data=json.dumps(data_to_post)
+        )
+
+        assert response.status_code == 400
+
+        assert response.json()['errors']
