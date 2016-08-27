6313c299d371d65d09d8c2f173f42a91ffbba65d
Michal Kononenko <michalkononenko@gmail.com>
2016-08-27 16:14:43 -0400

    Fixed bug in build script

--- topchef_client/setup.py
+++ topchef_client/setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 
-setup(name='topchef-client',
+setup(name='topchef_client',
       version='0.1',
       description='Python library for interfacing with the TopChef server',
       author='Michal Kononenko',
