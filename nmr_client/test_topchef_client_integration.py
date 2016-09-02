83bde8644a83fc667fbd6ad4675435d249558ee8
Michal Kononenko <michalkononenko@gmail.com>
2016-09-02 10:44:09 -0400

    New client is up and running. The first end-to-end test just passed

--- nmr_client/test_topchef_client_integration.py
+++ nmr_client/test_topchef_client_integration.py
@@ -13,12 +13,15 @@ from topchef_client import TopChefClient, NetworkManager
 from topchef_client import ValidationError
 from unit_test_runner import UnitTestRunner
 
+TEST_SERVER_ADDRESS = 'http://192.168.1.216/dev'
+TESTING_SERVICE_ID = '1cb40868-101f-11d9-9a55-000cf18a2ce6'
+
 True = "1"
 False = "0"
 
 class TestClient(unittest.TestCase):
 	def setUp(self):
-		self.host = 'http://192.168.1.39'
+		self.host = TEST_SERVER_ADDRESS
 		self.manager = NetworkManager(self.host)
 		self.client = TopChefClient(self.manager)
 		
@@ -75,7 +78,29 @@ class TestJSONSchemaValidation(TestClient):
 		self.assertRaises(
 			ValidationError, _thunk, self.client, self.invalid_object, self.schema
 		)
-
+		
+class TestGetServiceById(TestClient):
+	def setUp(self):
+		TestClient.setUp(self)
+		self.service_id = TESTING_SERVICE_ID
+	
+	def test_get_service_by_id(self):
+		service = self.client.get_service_by_id(self.service_id)
+		self.assertEqual(self.service_id, service.id)
+		
+class TestRequestJob(TestClient):
+	
+	def setUp(self):
+		TestClient.setUp(self)
+		self.service_id = TESTING_SERVICE_ID
+		
+		self.service = self.client.get_service_by_id(self.service_id)
+		self.job_parameters = {"value": 7}
+		
+	def test_request_job(self):
+		job = self.service.request_job(self.job_parameters)
+		
+		
 running_man = UnitTestRunner([
 	TestIsServerAlive('test_is_server_alive'),
 
@@ -84,6 +109,10 @@ running_man = UnitTestRunner([
 	TestLoopback('test_loopback'),
 	
 	TestJSONSchemaValidation('test_validate_schema'),
-	TestJSONSchemaValidation('test_invalid_schema')
+	TestJSONSchemaValidation('test_invalid_schema'),
+	
+	TestGetServiceById('test_get_service_by_id'),
+	
+	TestRequestJob('test_request_job')
 ])
 running_man.run_with_callback(MSG)
