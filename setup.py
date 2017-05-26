e6948889e06f78f965b44b8ccf0915ea237081fa
Michal Kononenko <michalkononenko@gmail.com>
2017-05-26 14:29:43 -0400

    Reduced the number of pip dependencies to a minimum by having the installation run from setuptools.

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='topchef',
       version='0.1',
       description='Python library for running remote computing jobs',
       author='Michal Kononenko',
       author_email='mkononen@uwaterloo.ca',
-      packages=['topchef']
+      packages=['topchef'],
+      install_requires=[
+          'Flask==0.12.2',
+          'jsonschema==2.6.0',
+          'marshmallow==2.13.5',
+          'marshmallow-jsonschema==0.3.0',
+          'pytz==2017.2',
+          'SQLAlchemy==1.1.10',
+          'sqlalchemy-migrate==0.11.0'
+      ]
 )
