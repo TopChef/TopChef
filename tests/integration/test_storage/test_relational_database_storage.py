c5eb0641e3e39881918c383e844e6172f0844c26
Michal Kononenko <michalkononenko@gmail.com>
2017-07-20 14:21:26 -0400

    Wrote unit tests for the relational DB storage.

--- tests/integration/test_storage/test_relational_database_storage.py
+++ tests/integration/test_storage/test_relational_database_storage.py
@@ -1,29 +1,52 @@
+"""
+Contains unit tests for the relational database storage
+"""
 import unittest
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from topchef.database.schemas import DatabaseSchemaWithJSONTable
 from topchef.storage import RelationalDatabaseStorage, JSONDocument
-from typing import Set
+from typing import Iterable
+from uuid import uuid4
 
 
 class TestRelationalDbStorage(unittest.TestCase):
     """
-    Test that the relational DB storage works correctly
+    Base class for unit testing the relational database storage
     """
-    def setUp(self):
-        self.engine = create_engine('sqlite://')
+    DATABASE_ENVIRONMENT_VARIABLE_KEY = 'DATABASE_URI'
+    SQLITE_IN_MEMORY_URI = 'sqlite://'
+
+    def setUp(self) -> None:
+        """
+        Create the required variables for the test, working out of the
+        database provided by the ``DATABASE_URI`` environment variable.
+        """
+        self.database_uri = os.environ.get(
+            self.DATABASE_ENVIRONMENT_VARIABLE_KEY,
+            default=self.SQLITE_IN_MEMORY_URI
+        )
+        self.engine = create_engine(self.database_uri)
         self.session_factory = sessionmaker(bind=self.engine)
         self.database = DatabaseSchemaWithJSONTable()
         self.database.metadata.create_all(bind=self.engine)
 
         self.storage = RelationalDatabaseStorage(self.session_factory)
 
-    def tearDown(self):
+    def tearDown(self) -> None:
+        """
+        Clear the database
+        """
         self.database.metadata.drop_all(bind=self.engine)
 
 
-class TestRelationalDbWithService(TestRelationalDbStorage):
-    def setUp(self):
+class TestRelationalDbWithDocument(TestRelationalDbStorage):
+    def setUp(self) -> None:
+        """
+
+        Create some JSON schemas, and add them to the database storage
+        """
         TestRelationalDbStorage.setUp(self)
         self.parameter_schema = {'type': 'object'}
         self.result_schema = {'type': 'result'}
@@ -35,7 +58,11 @@ class TestRelationalDbWithService(TestRelationalDbStorage):
             {self.parameter_document, self.result_document}
         )
 
-    def _add_documents_to_db(self, documents: Set[JSONDocument]) -> None:
+    def _add_documents_to_db(self, documents: Iterable[JSONDocument]) -> None:
+        """
+
+        :param documents: The documents to write
+        """
         session = self.session_factory()
 
         for document in documents:
@@ -44,14 +71,83 @@ class TestRelationalDbWithService(TestRelationalDbStorage):
         session.commit()
 
 
-class TestGetItem(TestRelationalDbWithService):
+class TestGetItemWithDocument(TestRelationalDbWithDocument):
     """
+    Contains unit tests for the document getter.
+    """
+    def setUp(self) -> None:
+        """
 
-    """
-    def test_getitem(self):
-        document_from_db = self.storage[self.parameter_document.id]  # type:
-        #  JSONDocument
+        Create an invalid document ID to test that finding invalid documents
+        doesn't work
+        """
+        TestRelationalDbWithDocument.setUp(self)
+        self.invalid_document_id = uuid4()
+
+    def test_getitem_document_exists(self) -> None:
+        """
+        Test that the document is correctly retrieved from storage if one
+        exists
+        """
+        document_from_db = self.storage[
+            self.parameter_document.id]  # type: JSONDocument
 
         self.assertEqual(
             document_from_db, self.parameter_schema
         )
+
+    def test_getitem_no_document(self) -> None:
+        """
+        Test that the system fails correctly
+        """
+        with self.assertRaises(KeyError):
+            _ = self.storage[self.invalid_document_id]
+
+
+class TestSetItem(TestRelationalDbWithDocument):
+    """
+    Contains unit tests for the document setter
+    """
+    def setUp(self):
+        """
+        Create an invalid ID to test the KeyError scenario, and some fake
+        document data that will be set in the new document
+        """
+        TestRelationalDbWithDocument.setUp(self)
+        self.invalid_document_id = uuid4()
+
+        self.new_document_data = {'foo': 'bar', 'bar': 'baz'}
+
+    def test_setitem(self):
+        """
+
+        Test that the setter works correctly
+        """
+        self.storage[self.parameter_document.id] = self.new_document_data
+        self.assertEqual(
+            self.new_document_data,
+            self.storage[self.parameter_document.id]
+        )
+
+    def test_setitem_no_document(self):
+        """
+
+        Test that the setter throws a ``KeyError`` if the document to be set
+        does not exist
+        """
+        with self.assertRaises(KeyError):
+            self.storage[self.invalid_document_id] = self.new_document_data
+
+
+class TestDelItem(TestRelationalDbWithDocument):
+    """
+    Contains unit tests for the deleter
+    """
+    def test_delitem(self):
+        """
+        Test that the document is properly deleted
+        """
+        del self.storage[self.parameter_document.id]
+
+        with self.assertRaises(KeyError):
+            _ = self.storage[self.parameter_document.id]
--- topchef/storage/relational_database_storage.py
+++ topchef/storage/relational_database_storage.py
@@ -19,11 +19,14 @@ class RelationalDatabaseStorage(DocumentStorage):
         """
 
         :param document_id: The ID of the document to retrieve
-        :return:
+        :return: The document with the desired ID
         """
         session = self._session_factory()  # type: Session
+        document_from_storage = self._get_document_from_storage(
+            session, document_id
+        )
 
-        return self._get_document_from_storage(session, document_id).document
+        return document_from_storage.document
 
     def __setitem__(
             self,
@@ -32,16 +35,11 @@ class RelationalDatabaseStorage(DocumentStorage):
     ) -> None:
         """
 
-        :param model_id:
-        :param new_json:
+        :param model_id: The ID of the document to set
+        :param new_json: The new JSON to be set
         """
         session = self._session_factory()  # type: Session
-
-        document = session.query(JSONDocument).filter_by(
-            document_id=model_id).first()  # type: Optional[JSONDocument]
-
-        if document is None:
-            raise KeyError('The document %s does not exist' % str(model_id))
+        document = self._get_document_from_storage(session, model_id)
 
         document.document = new_json
         session.add(document)
@@ -50,19 +48,26 @@ class RelationalDatabaseStorage(DocumentStorage):
     def __delitem__(self, model_id: UUID) -> None:
         """
 
-        :param model_id:
-        :return:
+        :param model_id: The ID of the document to delete
         """
         session = self._session_factory()  # type: Session
         document = self._get_document_from_storage(session, model_id)
         session.delete(document)
         self._safely_commit_session(session)
 
-    def __iter__(self) -> Iterable:
+    def __iter__(self) -> Iterable[UUID]:
+        """
+
+        :return: All the document IDs available
+        """
         session = self._session_factory()  # type: Session
         return session.query(JSONDocument.id).all()
 
     def __len__(self) -> int:
+        """
+
+        :return: The number of documents stored in this storage
+        """
         session = self._session_factory()  # type: Session
         return session.query(JSONDocument).count()
 
@@ -70,13 +75,27 @@ class RelationalDatabaseStorage(DocumentStorage):
     def _get_document_from_storage(
             session: Session, model_id: UUID
     ) -> JSONDocument:
+        """
+
+        :param session: The SQLAlchemy session to use for getting the document
+        :param model_id: The ID of the document to retrieve
+        :return: The model class containing the document
+        """
         document = session.query(JSONDocument).filter_by(
             id=model_id
         ).first()  # type: JSONDocument
+
+        if document is None:
+            raise KeyError('A document with ID %s does not exist' % model_id)
+
         return document
 
     @staticmethod
     def _safely_commit_session(session: Session) -> None:
+        """
+
+        :param session: The session to close
+        """
         try:
             session.commit()
         except:
